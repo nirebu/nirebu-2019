0a6c045847b31fb554fb7acc6287ad37684df148
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2019-04-05 01:58:42 +0200

    Added axios request to get the list of blog posts to populate the blog list. I will later change it to Vuex

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,20 @@
 <template>
+  <div>
   <LatestArticles />
+  {{ blogPosts }}
+  </div>
 </template>
 
 <script>
-import LatestArticles from '~/components/LatestArticles'
+import LatestArticles from '~/components/LatestArticles';
 
 export default {
   components: {
     LatestArticles
+  },
+  async asyncData({$axios}){
+    const blogPosts = await $axios.$get('/api/bloglist');
+    return {blogPosts}
   }
 }
 </script>
