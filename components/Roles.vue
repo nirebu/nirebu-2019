9f36c0c7a2149edc2cccf4405cd79aafcf606cb9
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2019-05-01 02:19:17 +0200

    Changed roles description

--- components/Roles.vue
+++ components/Roles.vue
@@ -8,32 +8,22 @@
               <h1 class="title nirebu-title has-background-gradient">#! /SysAdmin</h1>
               <p class="list-title has-text-primary has-text-weight-normal">Things I work on:</p>
               <ul>
-                <li>CentOS Linux</li>
                 <li>postfix</li>
                 <li>mongoDB</li>
-              </ul>
-              <p class="list-title has-text-primary has-text-weight-normal">Tools I use</p>
-              <ul>
+                <li>Docker</li>
                 <li>Ansible</li>
                 <li>Too many regex</li>
-                <li>bash</li>
               </ul>
             </div>
             <div class="column role">
               <h1 class="title nirebu-title has-background-gradient">&lt;WebDev /&gt;</h1>
-              <p class="list-title has-text-primary has-text-weight-normal">Things I'm learning:</p>
+              <p class="list-title has-text-primary has-text-weight-normal">Things that click with me:</p>
               <ul>
                 <li>Node.js backend</li>
                 <li>Reactive frontends with Vuejs</li>
-                <li>API development</li>
-              </ul>
-              <p
-                class="list-title has-text-primary has-text-weight-normal"
-              >Tools I'm trying to figure out</p>
-              <ul>
-                <li>SCSS</li>
+                <li>JAMStack</li>
                 <li>Bulma CSS framework</li>
-                <li>Webpack</li>
+                <li>Markdown</li>
               </ul>
             </div>
           </div>
