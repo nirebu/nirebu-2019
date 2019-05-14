57eec724c35337f4ea7ed201ceaa6d8edae44606
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2019-05-14 23:19:13 +0200

    Added Dev.to icon to footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -24,6 +24,7 @@ export default {
     return {
       socials: [
         { link: "https://github.com/nirebu", icon: "github" , label: "Check my Github profile" },
+        { link: "https://dev.to/nirebu", icon: "dev" , label: "My blog on Dev.to" },
         { link: "https://linkedin.com/in/nicolo-rebughini", icon: "linkedin" , label: "Fine me on Linkedin" },
         { link: "https://facebook.com/nicolo.rebughini", icon: "facebook" , label: "Find me on Facebook" },
         { link: "https://instagram.com/nirebu", icon: "instagram" , label: "Follow me on Instagram" }
